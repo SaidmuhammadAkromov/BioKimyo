147d251023f203b58de96c264a194d14381bf02a
SaidmuhammadAkromov <saidakromov2002@gmail.com>
2021-04-28 18:24:11 +0500

    screening header

--- header.js
+++ header.js
@@ -2,6 +2,6 @@ window.onload = function() {
     document.querySelector('.header-burger').addEventListener('click', function() {
         document.querySelector('.header-burger').classList.toggle('active')
         document.querySelector('.header-menu').classList.toggle('active')
-        document.querySelector('settings').classList.toggle('active')
+        document.querySelector('.settings').classList.toggle('active')
     })
 }
